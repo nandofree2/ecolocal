5b92319862cd9d5eb6b19aa33a2bccd19e20d32c
nandofree2 <nandofree2@gmail.com>
2025-12-16 21:33:50 +0700

    ntap

--- config/puma.rb
+++ config/puma.rb
@@ -1,42 +1,21 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+# config/puma.rb
 
-# Puma starts a configurable number of processes (workers) and each process
-# serves each request in a thread from an internal thread pool.
-#
-# The ideal number of threads per worker depends both on how much time the
-# application spends waiting for IO operations and on how much you wish to
-# to prioritize throughput over latency.
-#
-# As a rule of thumb, increasing the number of threads will increase how much
-# traffic a given process can handle (throughput), but due to CRuby's
-# Global VM Lock (GVL) it has diminishing returns and will degrade the
-# response time (latency) of the application.
-#
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
-#
-# Any libraries that use a connection pool or another resource pool should
-# be configured to provide at least as many connections as the number of
-# threads. This includes Active Record's `pool` parameter in `database.yml`.
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 min_threads_count = max_threads_count
 threads min_threads_count, max_threads_count
 
-environment ENV.fetch("RAILS_ENV", "production")
-
-bind "unix:///var/www/ecolocal/shared/tmp/sockets/puma.sock"
-
-pidfile "/var/www/ecolocal/shared/tmp/pids/puma.pid"
-state_path "/var/www/ecolocal/shared/tmp/pids/puma.state"
+environment ENV.fetch("RAILS_ENV", "development")
 
 workers ENV.fetch("WEB_CONCURRENCY", 1)
 preload_app!
 
-# Redirect STDOUT and STDERR to shared log files so we can inspect Puma errors
-stdout_redirect "/var/www/ecolocal/shared/log/puma.stdout.log", "/var/www/ecolocal/shared/log/puma.stderr.log", true
+if ENV["RAILS_ENV"] == "production"
+  bind "unix:///var/www/ecolocal/shared/tmp/sockets/puma.sock"
+
+  pidfile "/var/www/ecolocal/shared/tmp/pids/puma.pid"
+  state_path "/var/www/ecolocal/shared/tmp/pids/puma.state"
+else
+  port ENV.fetch("PORT", 3000)
+end
 
 plugin :tmp_restart
-
-
