0bd4a7a3ba54c8af5ecfaae89f2d3e32c4b69283
nandofree2 <nandofree2@gmail.com>
2025-12-17 12:06:23 +0700

    add config

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, "3.1.2"
 
 
-append :linked_files, "config/database.yml", "config/master.key"
+append :linked_files, "config/database.yml", "config/master.key", "config/credentials.yml.enc"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 set :puma_bind, "unix:///var/www/ecolocal/shared/tmp/sockets/puma.sock"
